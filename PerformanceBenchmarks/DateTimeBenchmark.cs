8828af00052d692b4f3e7b7e099b1ec5e88ae21f
msavara <hello@msavara.com>
2020-12-13 12:31:54 +0100

    Added DateTimeBenchmark

---
+++ PerformanceBenchmarks/DateTimeBenchmark.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace PerformanceBenchmarks
+{
+    [Config(typeof(Config))]
+    public class DateTimeBenchmark
+    {
+        private DateTime _dateNow;
+        private DateTimeOffset _dateOffset;
+
+        [Benchmark]
+        public void DateTime_Now()
+        {
+            _dateNow = DateTime.Now;
+        }
+
+        [Benchmark]
+        public void DateTime_Utc()
+        {
+            _dateNow = DateTime.UtcNow;
+        }
+
+        [Benchmark]
+        public void DateTimeOffset_Now()
+        {
+            _dateOffset = DateTimeOffset.Now;
+        }
+
+        [Benchmark]
+        public void DateTimeOffset_Utc()
+        {
+            _dateOffset = DateTimeOffset.UtcNow;
+        }
+    }
+}
